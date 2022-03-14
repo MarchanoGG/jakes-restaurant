35e38bd002d74dcdb8b913e6f2dee811e7023cc8
Jeremey <jeremeysas@gmail.com>
2022-03-14 20:57:07 +0100

    Added JSON file reader to Program (Read & ReadList) and added functional authentication logic

--- JakesRestaurant/controllers/ctlLogin.cs
+++ JakesRestaurant/controllers/ctlLogin.cs
@@ -4,19 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.Json;
 
 namespace Authentication
 {
     class TctlLogin
     {
-        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"data\", "auth.conf.txt");
-        StreamReader
+        static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"data\", "auth.conf.txt");
 
-        public bool Login() 
+        public bool Login(string aUserName, string aPassword) 
         {
             bool res = false;
 
-            Console.WriteLine(path);
+            List<TdoUser> users = JakesRestaurant.JsonFileReader.Read<TdoUser>(path);
+
+            TdoUser myUser = users.Find(i => i.Username == aUserName);
+
+            if (myUser != null)
+            {
+                if (myUser.Password == aPassword)
+                {
+                    res = true;
+                    Console.WriteLine("Successfully logged in.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid credentials provided!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials provided!");
+            }
 
             return res;
         }
