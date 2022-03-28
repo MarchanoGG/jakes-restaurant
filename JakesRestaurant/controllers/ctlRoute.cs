1a865399ae9527c8264f1d899cfcda98cd7f8c01
tobitotaku <99360631+tobitotaku@users.noreply.github.com>
2022-03-28 13:13:46 +0200

    added RouteController for handeling actions. Will be used later.

---
+++ JakesRestaurant/controllers/ctlRoute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakesRestaurant.controllers
+{
+    class ctlRoute
+    {
+        public bool InitAction()
+        {
+            bool res = true;
+            return res;
+        }
+    }
+}
