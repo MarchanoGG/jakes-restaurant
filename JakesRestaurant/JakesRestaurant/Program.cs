bd6c6b4efe0571ee215ee85dcee30fd1e36ec636
Michael <1028285@hr.nl>
2022-04-25 15:39:51 +0200

    Added Products and Removed doBase

---
+++ JakesRestaurant/JakesRestaurant/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace JakesRestaurant
+{
+    class Program
+    {
+        private static Authentication.User currentUser;
+        static Authentication.TctlLogin ctlAuth = new Authentication.TctlLogin();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jakes retaurant");
+            Console.WriteLine("Wijnhaven 107, 3011 WN in Rotterdam");
+            Console.WriteLine("Thema: <To be loaded from config> \r");
+
+            Console.WriteLine("\rDruk op een '1' om door te gaan naar de login");
+            Console.WriteLine("\rDruk op een '2' om een gebruiker aan te maken");
+
+            ConsoleKey key = Console.ReadKey().Key;
+
+            if (key == ConsoleKey.D1)
+            {
+                Console.Clear();
+                if (ctlAuth.Login())
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gebruiker: " + currentUser.Summary());
+
+                    // Main application options
+
+                    Console.ReadKey();
+                }
+            }
+            if(key == ConsoleKey.D2)
+            {
+                if (ctlAuth.CreateUser())
+                {
+                    Console.WriteLine("Gebruiker is aangemaakt");
+                }
+                else
+                {
+                    Console.WriteLine("Kan de gebruiker niet aanmaken!");
+                }
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
+            return;
+        }
+
+        static public void SetUser(Authentication.User aUser)
+        {
+           Program.currentUser = aUser;
+        }
+    }
+}
---
+++ JakesRestaurant/JakesRestaurant/controllers/ctlLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Authentication
+{
+    class TctlLogin
+    {
+        public bool Login() 
+        {
+            bool res = false;
+
+            User userCheck = new();
+            User user = userCheck.CheckCredentials(InsertCredentials("gebruikersnaam"), InsertCredentials("wachtwoord"));
+
+            if (user != null)
+            {
+                res = true;
+                JakesRestaurant.Program.SetUser(user);
+                Console.WriteLine("Succesvol aangemeld.");
+            }
+            else
+            {
+                Console.WriteLine("Onjuiste gegevens verstrekt!");
+            }
+
+            return res;
+        }
+        public bool CreateUser()
+        {
+            User user = new User();
+
+            Console.WriteLine("\n\rMaak een nieuwe gebruiker");
+
+            return user.CreateCredentials(InsertCredentials("gebruikersnaam"), InsertCredentials("wachtwoord"));
+        }
+
+        private static string InsertCredentials(string aCredential)
+        {
+            Console.WriteLine("\n\r");
+            Console.WriteLine("Voer " + aCredential + " in:");
+
+            return Console.ReadLine();
+        }
+
+    }
+}
