a9801f6021f8b5c52fd3d1bf5cebdf89369144a6
MarchanoGG <56337567+MarchanoGG@users.noreply.github.com>
2022-05-16 18:40:20 +0200

    Basic File Initialisation Added

--- JakesRestaurant/Program.cs
+++ JakesRestaurant/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using JakesRestaurant.views;
-
+using JakesRestaurant.controllers;
 namespace JakesRestaurant
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ctlInitialise initialise = new ctlInitialise();
+
             vLogin loginView = new vLogin();
 
             loginView.Navigation();
